ac4a3cd6bf5587c58088b801977c7434a7ea50d2
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-12-09 12:45:24 -0300

    Added express library and made a test

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello world")
+import express, {Request, Response} from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Hello world")
+})
+
+app.listen(3333)
